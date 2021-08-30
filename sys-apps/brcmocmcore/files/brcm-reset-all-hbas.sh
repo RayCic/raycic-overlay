ce3f71e6b82487ce558455384002dce793553463
Raimonds Cicans <ray@apollo.lv>
2021-08-30 15:08:31 +0300

    Yet another fix

--- sys-apps/brcmocmcore/files/brcm-reset-all-hbas.sh
+++ sys-apps/brcmocmcore/files/brcm-reset-all-hbas.sh
@@ -3,7 +3,13 @@
 
 brcmhbacmd="/opt/broadcom/brcmocmanager/bin/brcmhbacmd"
 
-macs=`${brcmhbacmd} listhba | grep 'Current MAC' | grep -oE '([[:xdigit:]]{2}-){5}[[:xdigit:]]{2}'`
+if [ -f /etc/broadcom/macs.conf ] ; then
+	echo "MACs file found. Reading MACs."
+	macs=`cat /etc/broadcom/macs.conf`
+else
+	echo "MACs file NOT found. Detecting MACs."
+	macs=`${brcmhbacmd} listhba | grep 'Current MAC' | grep -oE '([[:xdigit:]]{2}-){5}[[:xdigit:]]{2}'`
+fi
 
 for mac in ${macs} ; do
 	${brcmhbacmd} SetPortEnabled ${mac} 0
